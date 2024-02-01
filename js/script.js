a90e9d9e7420a926863daa2c910209e9cdda8d12
mallynGit <miguelitorruiz@gmail.com>
2024-02-01 08:33:15 +0100

    listo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-let app = document.getElementById('app');
+let app = document.getElementById("app");
+app.style = "border: 1px solid black; width: 90%; margin:auto;  display:flex; justify-content: space-evenly; flex-wrap:wrap;";
 
-let nextBtn = document.getElementById('nextBtn');
-let prevBtn = document.getElementById('prevBtn');
+let nextBtn = document.getElementById("nextBtn");
+let prevBtn = document.getElementById("prevBtn");
 
-
-let pokemon = [
-
-]
-
-
+let pokemon = [];
 
 // .then(res => res.json())
 // .then(data => {
@@ -16,22 +12,54 @@ let pokemon = [
 //     console.log(pokemon)
 // })
 
-window.onload = async () => {
-
-    let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
-    res = await res.json()
-    pokemon = res
-    console.log(pokemon)
-
-    nextBtn.setAttribute('href', res.next)
-
-    for (item of pokemon.results) {
-        console.log(item)
-        let div = document.createElement('div')
-        div.innerHTML = item.name;
-        div.className = 'pagination';
-        app.append(div);
-    }
+async function fetchPokemon(number) {
+  if (number == null) {
+    number = 0;
+  }
+  let res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${number * 10}`
+  );
+  res = await res.json();
+  pokemon = res;
+  return res;
 }
 
+function renderPokemon(array) {}
 
+window.onload = async () => {
+  let res = await fetchPokemon();
+  pokemon = res;
+  console.log(pokemon);
+
+  nextBtn.setAttribute("href", res.next);
+
+  let div = document.createElement("div");
+  div.className = "pagination";
+
+  for (item of pokemon.results) {
+    let poke = await fetch(item.url);
+    poke = await poke.json();
+    console.log(item, poke);
+
+    pokehtml = document.createElement("div");
+    // pokehtml.className = "pagination";
+
+    pokename = document.createElement("span");
+    pokename.innerHTML = item.name;
+    // pokename.style = "display: flex; justify-content: center;";
+    pokename.className = "pagination";
+
+    let pokeimage = document.createElement("img");
+    pokeimage.src = poke.sprites.other["official-artwork"].front_default;
+    pokeimage.style = "height: 15vw;";
+
+    pokehtml.appendChild(pokeimage);
+    pokehtml.appendChild(document.createElement("br"));
+    pokehtml.appendChild(pokename);
+    pokehtml.style = "border:1px solid black; width: 15vw;";
+
+    div.appendChild(pokehtml);
+    // app.appendChild(pokehtml);
+  }
+  app.append(div);
+};
