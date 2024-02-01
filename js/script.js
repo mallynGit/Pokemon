a589564209a4c4cd6a0653638ca61f70fde391e8
mallynGit <miguelitorruiz@gmail.com>
2024-02-01 13:20:42 +0100

    search and pagination and next/prev

--- js/script.js
+++ js/script.js
@@ -3,6 +3,52 @@ let app = document.getElementById("app");
 
 let nextBtn = document.getElementById("nextBtn");
 let prevBtn = document.getElementById("prevBtn");
+let resetBtn = document.getElementById("resetBtn");
+let searchInput = document.getElementById("searchInput");
+let searchButton = document.getElementById("searchBtn");
+
+let pagination = document.createElement("div");
+pagination.className = "pagination";
+pagination.style = "display: block; width: 100%; height:35px; text-align:center;"
+let pagNumber = document.createElement("input")
+pagNumber.type = "text"
+pagNumber.style = "text-align: center; width:35px; height: 30px; font-size: 20px";
+pagNumber.id = "pagNumber"
+pagNumber.value = 10
+pagination.append(pagNumber)
+
+pagNumber.addEventListener("change", async () => {
+    let e = await fetchPokemon(null, pagNumber.value)
+    if (e == "error") {
+        showError()
+    } else {
+        renderPokemon()
+    }
+})
+
+searchButton.addEventListener("click", async () => {
+    let e = await searchPokemon(searchInput.value)
+    if (e == "error") {
+        showError()
+    } else {
+        renderPokemon()
+    }
+})
+
+resetBtn.addEventListener("click", async () => {
+    await fetchPokemon()
+    renderPokemon()
+})
+
+nextBtn.addEventListener("click", async () => {
+    await fetchNext()
+    renderPokemon()
+})
+
+prevBtn.addEventListener("click", async () => {
+    await fetchPrevious()
+    renderPokemon()
+})
 
 let pokemon = [];
 
@@ -12,62 +58,134 @@ let pokemon = [];
 //     console.log(pokemon)
 // })
 
-async function fetchPokemon(number) {
-    if (number == null) {
-        number = 0;
-    }
-    let res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${number * 10}`
-    );
-    res = await res.json();
-    pokemon = res;
-    return res;
+function showError() {
+    app.innerHTML = ""
+    let span = document.createElement("span")
+    span.innerHTML = "Pokemon not found"
+    span.className = "error-message"
+    app.appendChild(span)
 }
 
-async function renderPokemon(array) {
+async function searchPokemon(name) {
+    console.log(name)
+    if (name == "") {
+        return "error"
+    }
+    try {
+        res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        res = await res.json()
+        console.log(res)
+        pokemon = res
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+async function fetchNext() {
+    await fetchPokemon(pokemon.next);
+}
+
+async function fetchPrevious() {
+    if (pokemon.previous == null) {
+
+    } else {
+        fetchPokemon(pokemon.previous)
+    }
+}
+
+async function fetchPokemon(offset, number) {
+    let res
+    console.log(offset,number,'desd efetcdh')
+    if (offset != null) {
+        res = await fetch(offset)
+    } else if(number==null){
+        res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+    }else {
+        res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=0`);
+    }
+    res = await res.json();
+    // pokemon = []
+    // for(result of res.results){
+    //     pokemon.push(result)
+    // }
+    pokemon = res;
+}
+
+async function renderPokemon() {
     app.innerHTML = "";
 
-    for (item of pokemon.results) {
-        let poke = await fetch(item.url);
-        poke = await poke.json();
-        console.log(item, poke);
+    console.log(pokemon)
+
+    if (pokemon.results) {
+        if (pokemon.results.length != undefined) {
+            for (item of pokemon.results) {
+
+                let poke = await fetch(item.url);
+
+                poke = await poke.json();
+
+
+                pokehtml = document.createElement("div");
+                // pokehtml.className = "pagination";
+                pokehtml.style = "width: 15vw; margin: 10px; box-sizing: border-box; text-align:center; background: white; padding:10px; user-select: none;";
+
+                pokename = document.createElement("p");
+                item.name = item.name[0].toUpperCase() + item.name.slice(1)
+                pokename.innerHTML = item.name;
+                pokename.style = "display: flex;justify-content: center;margin-top: 20px;"
+
+                let pokeimage = document.createElement("img");
+                pokeimage.src = poke.sprites.other["official-artwork"].front_default;
+                pokeimage.style = "width: 100%; height: auto;";
+
+                pokehtml.appendChild(pokeimage);
+                pokehtml.appendChild(pokename);
+
+
+                // div.appendChild(pokehtml);
+                app.appendChild(pokehtml);
+            }
+        }
+    } else {
+        console.log(pokemon);
+
 
         pokehtml = document.createElement("div");
         // pokehtml.className = "pagination";
-        pokehtml.style = "width: 15vw; margin: 10px; box-sizing: border-box;  text-align:center;border:1px solid black;    background: white; padding:10px; user-select: none;";
+        pokehtml.style = "width: 15vw; margin: 10px; box-sizing: border-box; text-align:center; background: white; padding:10px; user-select: none;";
 
         pokename = document.createElement("p");
-        item.name = item.name[0].toUpperCase() + item.name.slice(1)
-        pokename.innerHTML = item.name;
-        // pokename.style = "display: flex; justify-content: center;";
+        pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+        pokename.innerHTML = pokemon.name;
+        pokename.style = "text-weight: 900;";
         pokename.className = "pagination";
 
         let pokeimage = document.createElement("img");
-        pokeimage.src = poke.sprites.other["official-artwork"].front_default;
+        pokeimage.src = pokemon.sprites.other["official-artwork"].front_default;
         pokeimage.style = "width: 100%; height: auto;";
 
         pokehtml.appendChild(pokeimage);
-        pokehtml.appendChild(document.createElement("br"));
         pokehtml.appendChild(pokename);
 
 
         // div.appendChild(pokehtml);
         app.appendChild(pokehtml);
     }
-
 }
 
 window.onload = async () => {
-    let res = await fetchPokemon();
-    pokemon = res;
-    console.log(pokemon);
 
-    nextBtn.setAttribute("href", res.next);
+    await fetchPokemon()
+    console.log('before')
+    await renderPokemon()
+    console.log('after')
 
-    let div = document.createElement("div");
-    div.className = "pagination";
+    app.after(pagination)
+    // let div = document.createElement("div");
+    // div.className = "pagination";
 
-    renderPokemon()
+
 
     //   app.append(div);
 };
